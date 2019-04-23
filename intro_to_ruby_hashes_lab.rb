1c3eedb83774a170fdc909ab1c281e3c3bd07f81
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-23 01:25:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,10 @@ def monopoly_with_third_tier
       },
       :names=>
       {
-        :reading_railroad=>{}
+        :reading_railroad=>{},
+        :pennsylvania_railroad=>
+        {
+        },
       }
     }
   }
